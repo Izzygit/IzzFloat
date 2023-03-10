3264c204ee308f92e11f2fe7940de9e9378c265f
Izzygit <126716104+Izzygit@users.noreply.github.com>
2023-03-09 19:30:58 -0800

    Update float.c

--- float/float/float.c
+++ float/float/float.c
@@ -1852,10 +1852,10 @@ static void float_thd(void *arg) {
 			//stolen interface- Variable Tiltback rate //float surge_margin = 0.5; //Increased duty, in percent
 			float surge_margin = d->float_conf.tiltback_variable;
 			float surge_period = 1; //Period between each surge, in seconds. Prevents runaway and instability. 
-			//stolen interface- Nose Angling Speed //float surge_cycle = 0.1; //How much of the period with be at surge duty, in seconds
-			float surge_cycle = d->float_conf.noseangling_speed/1000; //UI in ms
-			//stolen interface- Constant Tiltback ERPM //float surge_ramp = 0.033; //How long until reaching 90% maximum surge duty, in seconds. 0 < surge_ramp <= surge_cycle
-			float surge_ramp = d->float_conf.tiltback_constant_erpm/1000; //UI in ms
+			//stolen interface- Turn Tiltback ERPM Threshold //float surge_cycle = 0.1; //How much of the period with be at surge duty, in seconds
+			float surge_cycle = d->float_conf.turntilt_start_erpm/1000; //UI in ms
+			//stolen interface- Nose Angling Speed //float surge_ramp = 0.033; //How long until reaching 90% maximum surge duty, in seconds. 0 < surge_ramp <= surge_cycle
+			float surge_ramp = d->float_conf.noseangling_speed/1000; //UI in ms
 			float surge_anglemin = 1; // Minimum d->proportional required to ensure we are continuously at an acceleration angle
 			float duty_increment = (float)(0.003085122* pow(surge_ramp, -0.9536787234)); //Formula to calc increment based on time to 90% target value at 832hz
 			//no longer used float new_duty_value = 0; 
